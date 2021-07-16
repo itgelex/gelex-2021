a61cee27abd0cf85ab21e3df0b2fa43db6c2d3fe
itgelex <87306945+itgelex@users.noreply.github.com>
2021-07-16 22:35:24 +0700

    Update youtube-player.js

--- youtube-player.js
+++ youtube-player.js
@@ -1,26 +1,35 @@
-  // 2. This code loads the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
+// 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
 
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    tag.src = "https://www.youtube.com/iframe_api";
+    tag.type = "text/javascript";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      width: '100%',
-      videoId: 'dAKQd2aKyZU',
-      playerVars: { 'autoplay': 1, 'playsinline': 1 },
-      events: {
-        'onReady': onPlayerReady
-      }
-    });
-  }
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+        width: '100%',
+        videoId: 'dAKQd2aKyZU',
+        playerVars: {
+          autoplay: 1,
+          playsinline: 1,
+          controls: 1, 
+          rel : 0,
+          fs : 0,
+          playlist: 'dAKQd2aKyZU',
+          loop: 1
+        },
+        events: {
+          'onReady': onPlayerReady
+        }
+      });
+    }
 
-  // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-     event.target.mute();
-    event.target.playVideo();
-  }
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+      event.target.mute();
+      event.target.playVideo();
+    }
